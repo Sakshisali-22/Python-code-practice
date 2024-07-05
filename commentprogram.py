7a1510f5107ca2257a33e6032861a8495df5c4ce
sakshi <sakshisali76@gmail.com>
2024-07-05 12:40:13 +0530

    Update code

---
+++ commentprogram.py
@@ -0,0 +1,5 @@
+print("hello sakshi")
+""""
+multi-line 
+comment
+"""
---
+++ thirdprogram.py
@@ -0,0 +1,12 @@
+#arithmetic operator 
+a = 10
+b = 10
+sum = a+b 
+print(sum)
+
+
+#relational operator 
+a == 50
+b == 20
+print(a==b)#False 
+print(a!=b)#True
