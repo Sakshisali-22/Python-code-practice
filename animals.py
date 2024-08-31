4bee5a6f7ac9f09fb39822c9ee0b9163028195f9
sakshi <sakshisali76@gmail.com>
2024-08-31 20:11:21 +0530

    six day program

---
+++ animals.py
@@ -0,0 +1,19 @@
+class animal:
+    pass
+
+class pets(animal):
+    pass
+
+class Dog(pets):
+    
+    
+    @staticmethod
+    def bark():
+        print("Bow Bow!")
+ 
+ 
+d = Dog()
+
+d.bark()   
+    
+    
